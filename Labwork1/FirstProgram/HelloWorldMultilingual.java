1b1558d9970a4636007473c6123f7e13fc9f630a
quanganh2001 <cocoi5659@gmail.com>
2022-10-11 08:43:56 +0700

    Print Hello World in as many human languages (English, French, Vietnamese)

---
+++ Labwork1/FirstProgram/HelloWorldMultilingual.java
@@ -0,0 +1,9 @@
+package FirstProgram;
+public class HelloWorldMultilingual {
+    public static void main(String[] args){
+        System.out.println("Hello World! (English)");
+        System.out.println("Salut le monde (French)");
+        System.out.println("Xin chào thế giới! (Tiếng Việt)");
+        System.out.println("안녕하세요 월드입니다! (Korean)");
+    }
+}
