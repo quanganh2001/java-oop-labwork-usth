2effb81e9a3f23a71426b1ea61b1cffe90ed70ba
quanganh2001 <cocoi5659@gmail.com>
2022-10-11 08:41:24 +0700

    Write a program that prints 10 Hello World ten times.

---
+++ Labwork1/FirstProgram/TenHelloWorld.java
@@ -0,0 +1,15 @@
+package FirstProgram;
+public class TenHelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+  }
+}
