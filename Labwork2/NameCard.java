79cfccf7bcc27b9bd91f4588d3d28351ceb81bda
quanganh2001 <cocoi5659@gmail.com>
2022-10-28 13:55:20 +0700

    Java class NameCard and a Java tester class NameCardTestDrive to present the corresponding NameCard design - Apply the “Encapsulation” concept in your classes - The tester class should print out the instance NameCard values

---
+++ Labwork2/NameCard.java
@@ -0,0 +1,20 @@
+// Write a Java class “NameCard” and a Java tester class “NameCardTestDrive”to present the corresponding NameCard design:
+// Apply the “Encapsulation” concept in your classes
+// The tester class should print out the instance NameCard values
+public class NameCard {
+    public String name;
+    public String phone;
+    public String email;
+
+    public void getName() {
+        System.out.println(name);
+    }
+
+    public void getPhone() {
+        System.out.println(phone);
+    }
+
+    public void getEmail() {
+        System.out.println(email);
+    }
+}
