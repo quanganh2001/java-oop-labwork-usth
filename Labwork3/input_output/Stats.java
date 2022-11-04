2fd9f8718879e3d6d941a8b4f420e20e7390f107
quanganh2001 <cocoi5659@gmail.com>
2022-11-04 16:58:20 +0700

    Write a program Stats.java that takes an integer command-line argument n, reads n floating-point numbers from standard input, and prints their mean (average value) and sample standard deviation (square root of the sum of the squares of their differences from the average, divided by n−1).

---
+++ Labwork3/input_output/Stats.java
@@ -0,0 +1,31 @@
+package Labwork3.input_output;
+
+public class Stats {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        double[] a = new double[n];
+
+        // read data and compute statistics
+        for (int i = 0; i < n; i++) {
+            a[i] = StdIn.readDouble();
+        }
+
+        // compute mean
+        double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            sum += a[i];
+        }
+        double mean = sum / n;
+
+        // compute standard deviation
+        double sum2 = 0.0;
+        for (int i = 0; i < n; i++) {
+            sum2 += (a[i] - mean) * (a[i] - mean);
+        }
+        double stddev = Math.sqrt(sum2 / (n - 1));
+
+        // print results
+        StdOut.println("Mean                      = " + mean);
+        StdOut.println("Sample standard deviation = " + stddev);
+    }
+}
