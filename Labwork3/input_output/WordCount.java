cd0deda6ff05f0b97489f1739948a96da58d8673
quanganh2001 <cocoi5659@gmail.com>
2022-11-04 16:59:02 +0700

    Write a program WordCount.java that reads in text from standard input and prints out the number of words in the text. For the purpose of this exercise, a word is a sequence of non-whitespace characters that is surrounded by whitespace.

---
+++ Labwork3/input_output/WordCount.java
@@ -0,0 +1,14 @@
+package Labwork3.input_output;
+
+public class WordCount {
+    public static void main(String[] args) {
+        int count = 0;
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            count++;
+        }
+
+        // output
+        StdOut.println("number of words = " + count);
+    }
+}
